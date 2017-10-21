f5008390b54ffea006b5fe3f4ac995663ceca788
Jack Zhao <JJack27@users.noreply.github.com>
2017-10-21 10:51:14 -0600

    Add two activities
    All activities still missing the xml layout files.
    
    Still miss the following fragments:
    - the 2nd tab in main activity (Trace)
    - the 3rd tab in main activity
    - the 4th tab in mian activity
    - the 2nd tab in eventActivity
    - the 3rd tab in eventActivity

---
+++ Grow/app/src/main/java/com/grow/cmputf17team4/grow/ActivityOtherUserProfile.java
@@ -0,0 +1,46 @@
+package com.grow.cmputf17team4.grow;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+/**
+ * Shows when user is viewing other user's profile
+ * @author Yizhou Zhao
+ */
+public class ActivityOtherUserProfile extends AppCompatActivity implements View.OnClickListener{
+
+    private Button following;
+    private boolean isFollowing;
+    private User otherUser;
+    private int otherUserId;
+    private int thisUserId;
+    private ArrayList<HabitEvent> otherUserHabits;
+    private TextView otherUserName;
+    private ListView habitList;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_other_user_profile);
+    }
+
+    @Override
+    public void onClick(View view){
+
+    }
+
+    /**
+     * let this user to follow other user. Add thisUserId into otherUser's Following array
+     * @param thisUserId current user's id
+     * @param otherUser the object of other user
+     */
+    public void followUser(int thisUserId, User otherUser){
+
+    }
+}
