66020090beaec48e2bd3e77ad28269e524a2bad5
Jack Zhao <JJack27@users.noreply.github.com>
2017-11-04 15:39:06 -0600

    changed architecture

--- Grow/app/src/main/java/com/grow/cmputf17team4/grow/ActivityHabitEvent.java
+++ Grow/app/src/main/java/com/grow/cmputf17team4/grow/ActivityHabitEvent.java
@@ -1,13 +1,10 @@
 package com.grow.cmputf17team4.grow;
 
-import android.support.annotation.NonNull;
+import android.os.Bundle;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.view.MenuItem;
-import android.view.MotionEvent;
-import android.view.View;
 
 /**
  * Activity to represent each habit event
@@ -24,74 +21,10 @@ public class ActivityHabitEvent extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_habit_event);
-        viewPager = (ViewPager) findViewById(R.id.viewpager_main);
-        bottomNavigationView = (BottomNavigationView) findViewById(R.id.bottom_navigation_main);
 
-        bottomNavigationView.setOnNavigationItemSelectedListener(
-                new BottomNavigationView.OnNavigationItemSelectedListener() {
-                    @Override
-                    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-                        switch (item.getItemId()) {
-                            case R.id.navigation_habit_event:
-                                viewPager.setCurrentItem(0);
-                                return false;
-                            case R.id.navigation_trace:
-                                viewPager.setCurrentItem(1);
-                                return false;
-                            case R.id.navigation_community:
-                                viewPager.setCurrentItem(2);
-                                return false;
-                            case R.id.navigation_settings:
-                                viewPager.setCurrentItem(3);
-                                return false;
-                            default:
-                                return false;
-                        }
-                    }
-                });
-
-        viewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
-            @Override
-            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
-
-            }
-
-            @Override
-            public void onPageSelected(int position) {
-                if (menuItem != null) {
-                    menuItem.setChecked(false);
-                } else {
-                    bottomNavigationView.getMenu().getItem(0).setChecked(false);
-                }
-                menuItem = bottomNavigationView.getMenu().getItem(position);
-                menuItem.setChecked(true);
-            }
-
-            @Override
-            public void onPageScrollStateChanged(int state) {
-            }
-        });
-
-
-        viewPager.setOnTouchListener(new View.OnTouchListener() {
-            @Override
-            public boolean onTouch(View v, MotionEvent event) {
-                return true;
-            }
-        });
-
-        setupViewPager(viewPager);
     }
 
-    private void setupViewPager(ViewPager viewPager) {
-        ViewPagerAdapter adapter = new ViewPagerAdapter(getSupportFragmentManager());
 
-        adapter.addFragment(FragmentEventList.newInstance("Habit Event"));
-        adapter.addFragment(FragmentEventList.newInstance("Trace"));
-        adapter.addFragment(FragmentEventList.newInstance("Community"));
-        adapter.addFragment(FragmentEventList.newInstance("Settings"));
-        viewPager.setAdapter(adapter);
-    }
 
 
 
--- Grow/app/src/main/java/com/grow/cmputf17team4/grow/FragmentUserSetting.java
+++ Grow/app/src/main/java/com/grow/cmputf17team4/grow/FragmentUserSetting.java
@@ -6,22 +6,10 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.RadioGroup;
 import android.widget.TextView;
 
-/**
- * Created by yizho on 2017/10/21.
- */
+public class FragmentUserSetting extends Fragment {
 
-public class FragmentHabitSetting extends Fragment{
-
-    private EditText habitName;
-    private EditText reasonToPlay;
-    private Button restart;
-    private Button delete;
-    private RadioGroup routine;
 
     public static FragmentEventList newInstance(String info) {
         Bundle args = new Bundle();
@@ -34,18 +22,10 @@ public class FragmentHabitSetting extends Fragment{
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_habit_settings, null);
+        View view = inflater.inflate(R.layout.fragment_event_list_today, null);
         TextView tvInfo = (TextView) view.findViewById(R.id.textView);
         tvInfo.setText(getArguments().getString("info"));
 
         return view;
-    }
-
-    public void saveInFile(){
-
-    }
-
-    public void inToggle(View view){
-        
     }
 }
