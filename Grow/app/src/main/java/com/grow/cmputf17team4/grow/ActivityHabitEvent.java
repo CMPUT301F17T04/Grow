11d5cda3e109ebaeead26dce4f02a1413c276c87
Jack Zhao <JJack27@users.noreply.github.com>
2017-11-04 15:44:37 -0600

    Added comment

--- Grow/app/src/main/java/com/grow/cmputf17team4/grow/ActivityHabitEvent.java
+++ Grow/app/src/main/java/com/grow/cmputf17team4/grow/ActivityHabitEvent.java
@@ -1,21 +1,12 @@
 package com.grow.cmputf17team4.grow;
 
 import android.os.Bundle;
-import android.support.design.widget.BottomNavigationView;
-import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
-import android.view.MenuItem;
 
 /**
  * Activity to represent each habit event
  */
 public class ActivityHabitEvent extends AppCompatActivity {
-
-    private ViewPager viewPager;
-    private MenuItem menuItem;
-    private User user;
-    private String file;
-    private BottomNavigationView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
