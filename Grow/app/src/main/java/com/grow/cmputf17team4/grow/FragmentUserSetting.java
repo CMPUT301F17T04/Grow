a42d1403b81329f699a7c4495491e7bfc4290db2
cassie <qin7@ualberta.ca>
2017-11-05 16:54:23 -0700

    fix minor bugs

--- Grow/app/src/main/java/com/grow/cmputf17team4/grow/FragmentUserSetting.java
+++ Grow/app/src/main/java/com/grow/cmputf17team4/grow/FragmentUserSetting.java
@@ -7,6 +7,8 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.Switch;
 import android.widget.TextView;
 
