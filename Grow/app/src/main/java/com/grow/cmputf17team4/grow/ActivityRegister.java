cd05b0482f6020aa0d6f345c92dce300c4156090
AeolusFS <zhai@ualberta.ca>
2017-11-08 12:33:08 +0800

    register

--- Grow/app/src/main/java/com/grow/cmputf17team4/grow/ActivityRegister.java
+++ Grow/app/src/main/java/com/grow/cmputf17team4/grow/ActivityRegister.java
@@ -6,11 +6,13 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class ActivityRegister extends AppCompatActivity {
 
     private EditText username;
     private Button register;
+    private String nameString;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,9 +25,50 @@ public class ActivityRegister extends AppCompatActivity {
         register.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(ActivityRegister.this, ActivityLogin.class);
-                startActivity(intent);
+                nameString = username.getText().toString();
+                //uid = userList.getCount();
+                boolean sign = true;
+                if (username.length() == 0){
+                    username.setError("User name cannot be empty");
+                    sign = false;
+                }
+
+                User user = new User(nameString);
+                if (existedUser(nameString)) {
+                    try{
+                        Toast.makeText(getApplicationContext(),"Duplicate user name!", Toast.LENGTH_LONG).show();
+                        throw new IllegalArgumentException("Duplicate user");
+                    }catch(IllegalArgumentException e){
+
+                    }
+                }
+
+                else {
+                    //userList.addUser(user);
+                    ElasticSearchHelper.AddUserTask addUserTask
+                            = new ElasticSearchHelper.AddUserTask();
+                    addUserTask.execute(user);
+                    if (sign) {
+                        Intent intent = new Intent(ActivityRegister.this, ActivityLogin.class);
+                        startActivity(intent);
+                    }
+                }
             }
         });
+    }
+
+    private boolean existedUser (String name) {
+        ElasticSearchHelper.IsExist isExist = new ElasticSearchHelper.IsExist();
+        isExist.execute(name);
+
+        try {
+            if (isExist.get()) {
+                return true;
+            } else {
+                return false;
+            }
+        } catch (Exception e) {
+            return false;
+        }
     }
 }
