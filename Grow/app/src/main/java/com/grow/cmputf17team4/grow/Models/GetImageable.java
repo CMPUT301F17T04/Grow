1507c049edb1bc6d6b33ba4b3243f63177eaf01d
cassie <qin7@ualberta.ca>
2017-12-01 17:09:38 -0700

    fix name

---
+++ Grow/app/src/main/java/com/grow/cmputf17team4/grow/Models/GetImageable.java
@@ -0,0 +1,10 @@
+package com.grow.cmputf17team4.grow.Models;
+
+/**
+ * Created by qin7 on 2017/12/1.
+ */
+
+public interface GetImageable {
+    String getEncodedImage();
+    void setEncodedImage(String encodedImage);
+}
