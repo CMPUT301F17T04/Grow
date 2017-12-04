63acb751d6fedbad9116518ff667b5650d0ef718
JJack27 <yizhouzhao27@gmail.com>
2017-12-04 00:45:40 -0700

    initial push

---
+++ Grow/app/src/main/java/com/grow/cmputf17team4/grow/Views/ActivityFollowingHabits.java
@@ -0,0 +1,15 @@
+package com.grow.cmputf17team4.grow.Views;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.grow.cmputf17team4.grow.R;
+
+public class ActivityFollowingHabits extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_following_habits);
+    }
+}
